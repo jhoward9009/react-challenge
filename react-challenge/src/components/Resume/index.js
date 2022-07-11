9e6eee44c15fe453e7a58890ecb6f3d7ad297d31
jhoward9009 <j.how9009@gmail.com>
2022-07-11 06:35:04 -0400

    edit resume

--- react-challenge/src/components/Resume/index.js
+++ react-challenge/src/components/Resume/index.js
@@ -5,6 +5,7 @@ function Resume() {
 
     return (
         <div className='about-me'>
+            <h2>Resume</h2>
             <p>
                 I am a jack of all trades. I have done a little bit of everything.
             </p>
