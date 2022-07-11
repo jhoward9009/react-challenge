93629ed6433963ab98301dde5b4e802e95dc2e39
jhoward9009 <j.how9009@gmail.com>
2022-07-11 06:21:25 -0400

    added resume

---
+++ react-challenge/src/components/Resume/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Resume() {
+
+    return (
+        <div className='about-me'>
+            <p>
+                I am a jack of all trades. I have done a little bit of everything.
+            </p>
+        </div>
+
+    )
+}
+
+export default Resume
